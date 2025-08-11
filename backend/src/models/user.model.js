e6aa208075a608c21b39acdd042efd24e3f0544f
Harsh Pandey <harshpandey1924@gmail.com>
2025-08-11 21:00:03 +0530

    Clean branch for Render deploy

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,11 @@
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  token: { type: String },
+});
+
+export default mongoose.model("User", userSchema); // ✅ default export
---
+++ frontend/src/environment.js
@@ -0,0 +1,8 @@
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://zoom-clone-dka8.onrender.com" :
+
+    "http://localhost:8000"
+
+
+export default server;
